6549f464ca123050fffb8b04dc22a2f2f92edab0
Felipe Quintero <ffquint@gmail.com>
2021-04-10 21:48:39 -0400

    added an addtl line for public repos # and link, also moved bio and upated colors for styling.

--- github-user-card/src/components/User.js
+++ github-user-card/src/components/User.js
@@ -31,15 +31,19 @@ class User extends React.Component {
                     <Card.Text>{user.location}</Card.Text>
 
                     <Card.Text>
+                        <Badge variant="warning">Bio: {user.bio}</Badge>
+                    </Card.Text>
+
+                    <Card.Text>
                         <Badge variant="warning">Profile: <a href={user.html_url}>{user.html_url}</a></Badge>
                     </Card.Text>
                     
                     <Card.Text>
                         <Badge variant="warning">Following: {user.following}</Badge>
                     </Card.Text>
-                    
+
                     <Card.Text>
-                        <Badge variant="warning">Bio: {user.bio}</Badge>
+                        <Badge variant="warning">Public Repos: {user.public_repos} <br/><br/> <a href={user.repos_url}>{user.repos_url}</a></Badge>
                     </Card.Text>
                 </Card.Body>
                 </Card>
